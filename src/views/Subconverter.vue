69318446dbb5e45a0f36230b93ec67281b0e237d
lanneo <65434192+lanneo@users.noreply.github.com>
2026-04-06 09:33:21 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,12 +348,14 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
+          "自用专属后端": "https://subconv.xn--4gqr29o.eu.org",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
           "肥羊提供-备用后端": "https://api.v1.mk",
         },
         backendOptions: [
+          { value: "https://subconv.xn--4gqr29o.eu.org" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
